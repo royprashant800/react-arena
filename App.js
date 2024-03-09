81f0887f8daac91520d03ae0156cb256d3eed617
Prashant <royprashant800@gmail.com>
2024-03-09 23:00:12 +0530

    commmit initial changes

---
+++ App.js
@@ -0,0 +1,5 @@
+const heading = React.createElement("h1", {id : "head", xyz : "abc"}, "Hello world using React");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
+
