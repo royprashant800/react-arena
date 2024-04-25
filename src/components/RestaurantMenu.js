ea514c0582e36ea408484916e7c4ddde8800c555
Prashant <royprashant800@gmail.com>
2024-04-25 23:08:26 +0530

    fetched restaurant menu

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -10,25 +10,29 @@ const RestaurantMenu = () => {
     }, []);
 
     const fetchMenu = async () => {
-        const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=23.1808466&lng=72.6471847&restaurantId=45093&catalog_qa=undefined&isMenuUx4=true");
+        const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=23.1808466&lng=72.6471847&restaurantId=605457&catalog_qa=undefined&isMenuUx4=true");
 
         const json = await data.json();
         setResInfo(json.data)
         console.log(json);
     }
 
-    // const { costForTwoMessage } = resInfo?.cards[2]?.card?.card?.info;
+    if(resInfo === null) return <Shimmer />;
 
-    return resInfo === null ? <Shimmer /> : (
+    const { name,cuisines,costForTwoMessage } = resInfo?.cards[2]?.card?.card?.info;
+
+    const { itemCards } = resInfo.cards[4].groupedCard.cardGroupMap.REGULAR.cards[1].card.card;
+    console.log(itemCards[0].card.info.name);
+
+    return (
         <div className="menu">
-            <h1>{resInfo?.cards[2]?.card?.card?.info?.name}</h1>
-            <h3>{resInfo?.cards[2]?.card?.card?.info?.cuisines.join(",")}</h3>
-            <h3>{resInfo?.cards[2]?.card?.card?.info?.costForTwoMessage}</h3>
+            <h1>{name}</h1>
+            <h3>{cuisines.join(",")}</h3>
+            <h3>{costForTwoMessage}</h3>
             <ul>
-                <li>Burger</li>
-                <li>Sandwich</li>
-                <li>Pauva</li>
-                <li>Paneer Kadhai</li>
+                {itemCards.map((item) => ( 
+                    <li key={item.card.info.id}>{item.card.info.name} - {item.card.info.price/100}</li>
+                ))}
             </ul>
         </div>
     )
