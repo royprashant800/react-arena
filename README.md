ba60653839d8ca58a40a5135b09411c609c0ca1d
Prashant <royprashant800@gmail.com>
2024-09-20 17:31:37 +0530

    restaurant menu

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # react-arena
+
+
+Testing:
+1. Unit testing
+2. Integration testing
+3. E2E testing
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -6,7 +6,7 @@ const ItemList = ({ items }) => {
                 <div key={item.card.info.id} className="p-2 m-2 border-gray-300 border-b-2 text-left">
                     <div className="py-2">
                         <span>{item.card.info.name}</span>
-                        <span> - ₹{item.card.info.price/100}</span>
+                        <span> - ₹{item.card.info.price ? item.card.info.price/100 : item.card.info.defaultPrice/100}</span>
                     </div>
                     <p className="text-sm">{item.card.info.description}</p>
                 </div>
